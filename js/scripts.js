8ca88b41b4e5018c31572855e147995ad13f7c5b
Alex Johnson <alex.johnson293@gmail.com>
2022-10-26 14:32:01 -0700

    initialize project structure & first test
    Co-authored-by: Dominik Magic <dominikmagic5@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+function toPigLatin() {
+//grab input & declare variables
+  // qu permutations
+  //quick -> ickquay
+  //grab qu store as suffix, assign remaining to prefix
+  //concat 'ay'
+
+//if string contains no vowels
+// string plus way
+
+//elif string starts with consonants
+  // code -> odecay
+  // select initial consonant and store as suffix, assign remaining to prefix
+
+//Test: "It will add 'way' to the end of words that begin with a vowel."
+  //concat + add 'ay'
+  //elif string starts with vowels (DO NOT use 'y' as vowel)
+  //add 'way'
+
+
+}
---
+++ readme.md
@@ -0,0 +1,5 @@
+Describe: toPigLatin();
+
+Test: "It will add 'way' to the end of words that begin with a vowel."
+Code: toPigLatin("a");
+Expected Output: "away"
